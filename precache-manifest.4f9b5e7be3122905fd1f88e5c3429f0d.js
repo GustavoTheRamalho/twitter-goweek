d21363fa3fea40a2e4c3f2df4740bac3efdcb018
GusRamalho <31655649+GustavoTheRamalho@users.noreply.github.com>
2018-11-11 10:59:03 -0200

    Updates

---
+++ precache-manifest.4f9b5e7be3122905fd1f88e5c3429f0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bde2a6d130abc251ae74292ae431e3e",
+    "url": "/twitter-goweek/static/media/twitter.7bde2a6d.svg"
+  },
+  {
+    "revision": "c71630b0a771abe9b829bceaecacdcba",
+    "url": "/twitter-goweek/static/media/like.c71630b0.svg"
+  },
+  {
+    "revision": "b39fd3951679b7173c7f",
+    "url": "/twitter-goweek/static/js/runtime~main.b39fd395.js"
+  },
+  {
+    "revision": "73b2ab41a9c11c2e2fa6",
+    "url": "/twitter-goweek/static/js/main.73b2ab41.chunk.js"
+  },
+  {
+    "revision": "348b987d1213ca42fe0f",
+    "url": "/twitter-goweek/static/js/1.348b987d.chunk.js"
+  },
+  {
+    "revision": "73b2ab41a9c11c2e2fa6",
+    "url": "/twitter-goweek/static/css/main.91135330.chunk.css"
+  },
+  {
+    "revision": "93179d7a756c8eddbfb4a4d10edf89a0",
+    "url": "/twitter-goweek/index.html"
+  }
+];
